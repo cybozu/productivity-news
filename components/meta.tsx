ec42ef49e58e76bb2d86ebd37710f3f5026a0250
Hideki Igarashi <hideki.develop@gmail.com>
2022-04-06 20:29:07 +0900

    faviconをオリジナルのものに変更

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,15 +4,14 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 const Meta = () => {
   return (
     <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
+      <link rel="icon" href="/favicon/favicon.ico" />
+      <link rel="icon" href="/favicon/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="theme-color" content="#000" />
+
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+
+      <meta name="theme-color" content="#000" />
       <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
