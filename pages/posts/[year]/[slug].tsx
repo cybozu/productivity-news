9528bf5f67ac04c5ef4146c40e4d3858f42896f0
Hideki Igarashi <hideki.develop@gmail.com>
2022-04-06 14:00:02 +0900

    ESLintの修正を適用

--- pages/posts/[year]/[slug].tsx
+++ pages/posts/[year]/[slug].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
+import Head from "next/head";
 import Container from "../../../components/container";
 import PostBody from "../../../components/post-body";
 import Header from "../../../components/header";
@@ -7,7 +8,6 @@ import PostHeader from "../../../components/post-header";
 import Layout from "../../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../../lib/api";
 import PostTitle from "../../../components/post-title";
-import Head from "next/head";
 import { CMS_NAME } from "../../../lib/constants";
 import markdownToHtml from "../../../lib/markdownToHtml";
 import PostType from "../../../types/post";
