7adf27b8e4689a753f9f3c654111467e21226784
Hideki Igarashi <hideki.develop@gmail.com>
2022-04-12 21:10:40 +0900

    basePathを環境変数で差し替えできるように設定

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const basePath = process.env.BASE_PATH || "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,6 +7,11 @@ const nextConfig = {
   // export時に「/pages/sample/index.tsx」を「/sample.html」ではなく、
   // 「/sample/index.html」となるようにし、「/sample/」アクセスできるようにする。
   trailingSlash: true,
+
+  // リポジトリ単位のGitHub Pagesはルートが「/」ではなくリポジトリ名になる。
+  // ローカル開発での利便性を考慮して環境変数で差し替え可能にする。
+  assetPrefix: basePath,
+  basePath,
 };
 
 module.exports = nextConfig;
