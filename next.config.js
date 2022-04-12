1a58c5a4cd74b6527ae1719561af594ab96e8fa3
Hideki Igarashi <hideki.develop@gmail.com>
2022-04-12 14:01:17 +0900

    Trailing Slashを有効化

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // export時に「/pages/sample/index.tsx」を「/sample.html」ではなく、
+  // 「/sample/index.html」となるようにし、「/sample/」アクセスできるようにする。
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
